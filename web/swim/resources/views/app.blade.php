f7a619c672a24e9362830f2e830876e7bd7ee102
TalGhar <no.merci420@gmail.com>
2023-06-28 18:00:07 +0300

    web rgr > added tailwindcss dependencies

--- web/swim/resources/views/app.blade.php
+++ web/swim/resources/views/app.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Плыви!</title>
-
+    @vite('resources/css/app.css')
     @viteReactRefresh
     @vite('resources/js/index.tsx')
 </head>
