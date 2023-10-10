8e1e6601daf897f0a08e779ade4e148a87a3c62f
TalGhar <pr1tzeg@yandex.ru>
2023-10-10 13:26:55 +0300

    7 sem init

---
+++ 7sem/CrossPlatform/lab2/2nd/MyClass.h
@@ -0,0 +1,21 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+#include <QLabel>
+
+class MyLabel : public QLabel
+{
+    Q_OBJECT
+private:
+
+public:
+
+public slots:
+
+signals:
+
+};
+
+#endif // MYCLASS_H
+
+
---
+++ 7sem/MISII/lab1/app/task1.py
@@ -0,0 +1,41 @@
+#format
+print("{0} {1}".format("Example", "of format method"))
+print("just float => {0} | float with width=12 and 2 symbs after . => {0:012.2f}".format(123.12312))
+print("numbers with {0:+f} {1: f}".format(12.123, -12.123))
+print("Align with format method:")
+print("{:5}".format("asd"))
+print("{:5}".format("asd"))
+print("{:>5}".format("asd"))
+print("{:= 6.3f}".format(123.12312))
+
+#strip
+
+#lstrip 
+
+#rstrip 
+
+#capitalize 
+
+#title 
+
+#count
+
+#index
+
+#rindex
+
+#startswith
+
+#endswith
+
+#replace 
+
+#split 
+
+#rsplit 
+
+#join 
+
+#partition 
+
+#rpartition
